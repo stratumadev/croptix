97b59eeb401a73aa5ec363bd4e6a8158c9945cea
stratumadev <stratumst@proton.me>
2026-03-18 13:17:03 +0100

    tempfix katamari player replacement

--- src/background.ts
+++ src/background.ts
@@ -27,3 +27,31 @@ browser.webRequest.onBeforeRequest.addListener(
     },
     ['blocking']
 )
+
+// Replace bundle.js request body with modified bundle.js content
+browser.webRequest.onBeforeRequest.addListener(
+    (details) => {
+        const filter = browser.webRequest.filterResponseData(details.requestId)
+        const encoder = new TextEncoder()
+        filter.ondata = () => {}
+
+        filter.onstop = async () => {
+            try {
+                const localScript = await fetch(browser.runtime.getURL('katamari-fix.js')).then((r) => r.text())
+
+                filter.write(encoder.encode(localScript))
+            } catch (e) {
+                console.error('Chunk replace failed', e)
+            } finally {
+                filter.close()
+            }
+        }
+
+        return {}
+    },
+    {
+        urls: ['https://www.crunchyroll.com/build/_next/static/chunks/8134-d35297e852da455a.js'],
+        types: ['script']
+    },
+    ['blocking']
+)
