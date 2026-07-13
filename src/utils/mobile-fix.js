266f0481105f6d85bdbccb260b581fc6f118f785
iArisu <68963282+iArisu@users.noreply.github.com>
2026-07-13 17:44:24 +0200

    Try to be stealth, navigator.userAgent made configurable

--- src/utils/mobile-fix.js
+++ src/utils/mobile-fix.js
@@ -1,3 +1,5 @@
+// Note: one day we may have to mock navigator.userAgentData
 Object.defineProperty(navigator, 'userAgent', {
-    get: () => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36'
-});
+    get: () => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36',
+    configurable: true
+})
